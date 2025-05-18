2d59c6cf66b8663dd00c26bb5065bb6c10bd2412
Kinland0 <egornikolaev-2017@yandex.ru>
2025-05-18 11:01:39 +0400

    Определение базовых классов иерархии Expression

--- trpo_lab6_task1-2.cpp
+++ trpo_lab6_task1-2.cpp
@@ -28,3 +28,22 @@ struct Transformer {
     virtual Expression* transformFunctionCall(FunctionCall const*) = 0;
     virtual Expression* transformVariable(Variable const*) = 0;
 };
+
+
+struct Number : Expression {
+    Number(double value) : value_(value) {}
+    double value() const { return value_; }
+    double evaluate() const override { return value_; }
+    Expression* transform(Transformer* tr) const override { return tr->transformNumber(this); }
+private:
+    double value_;
+};
+
+struct Variable : Expression {
+    Variable(string const& name) : name_(name) {}
+    string const& name() const { return name_; }
+    double evaluate() const override { return 0.0; }
+    Expression* transform(Transformer* tr) const override { return tr->transformVariable(this); }
+private:
+    string const name_;
+};
