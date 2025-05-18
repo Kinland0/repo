729d9350795a56d7f1d5a7c20fa4ed86a438ffbb
Kinland0 <egornikolaev-2017@yandex.ru>
2025-05-18 10:59:53 +0400

    Определение паттерна Visitor

--- trpo_lab6_task1-2.cpp
+++ trpo_lab6_task1-2.cpp
@@ -19,3 +19,12 @@ struct Expression {
     virtual double evaluate() const = 0;
     virtual Expression* transform(class Transformer* tr) const = 0;
 };
+
+
+struct Transformer {
+    virtual ~Transformer() {}
+    virtual Expression* transformNumber(Number const*) = 0;
+    virtual Expression* transformBinaryOperation(BinaryOperation const*) = 0;
+    virtual Expression* transformFunctionCall(FunctionCall const*) = 0;
+    virtual Expression* transformVariable(Variable const*) = 0;
+};
