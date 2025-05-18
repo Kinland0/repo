b20abf749f8551c38665e4d1fbab798a336f3606
Kinland0 <egornikolaev-2017@yandex.ru>
2025-05-18 11:02:22 +0400

    Реализация структур BinaryOperation и FunctionCall

--- trpo_lab6_task1-2.cpp
+++ trpo_lab6_task1-2.cpp
@@ -47,3 +47,47 @@ struct Variable : Expression {
 private:
     string const name_;
 };
+
+
+struct BinaryOperation : Expression {
+    enum { PLUS = '+', MINUS = '-', DIV = '/', MUL = '*' };
+    BinaryOperation(Expression const* left, int op, Expression const* right)
+        : left_(left), op_(op), right_(right) {
+        assert(left_ && right_);
+    }
+    ~BinaryOperation() { delete left_; delete right_; }
+    Expression const* left() const { return left_; }
+    Expression const* right() const { return right_; }
+    int operation() const { return op_; }
+    double evaluate() const override {
+        double l = left_->evaluate(), r = right_->evaluate();
+        switch (op_) {
+        case PLUS: return l + r; case MINUS: return l - r;
+        case DIV: return l / r; case MUL: return l * r;
+        }
+        return 0;
+    }
+    Expression* transform(Transformer* tr) const override { return tr->transformBinaryOperation(this); }
+private:
+    Expression const* left_;
+    Expression const* right_;
+    int op_;
+};
+
+struct FunctionCall : Expression {
+    FunctionCall(string const& name, Expression const* arg)
+        : name_(name), arg_(arg) {
+        assert(arg_); assert(name_ == "sqrt" || name_ == "abs");
+    }
+    ~FunctionCall() { delete arg_; }
+    string const& name() const { return name_; }
+    Expression const* arg() const { return arg_; }
+    double evaluate() const override {
+        double val = arg_->evaluate();
+        return (name_ == "sqrt") ? sqrt(val) : fabs(val);
+    }
+    Expression* transform(Transformer* tr) const override { return tr->transformFunctionCall(this); }
+private:
+    string const name_;
+    Expression const* arg_;
+};
